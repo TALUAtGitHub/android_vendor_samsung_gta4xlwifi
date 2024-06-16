1e6f978a7636f42e689f1e1dedddf8aa12265811
K9100ii <k9100ii.ki@gmail.com>
2024-06-16 18:28:11 +0100

    gta4xlwifi: Update blobs & firmware from P610XXS5FXE4
    Change-Id: I3ebc8cf2a9a5615144397719f48a14b1b07dbe08

--- Android.mk
+++ Android.mk
@@ -6,12 +6,12 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),gta4xlwifi)
 
-$(call add-radio-file-sha1-checked,radio/cm.bin_SM-P610,f1fe2542e2ef98342228e396751babca20dc7404)
-$(call add-radio-file-sha1-checked,radio/keystorage.bin_SM-P610,959f35fda2db62371bd23e5eb693ea72fb3f351f)
+$(call add-radio-file-sha1-checked,radio/cm.bin_SM-P610,7df927ba1e454a94692310e41bdefc166b55906f)
+$(call add-radio-file-sha1-checked,radio/keystorage.bin_SM-P610,b60ce082f655277c54d8deadd6849ebcb035bdc9)
 $(call add-radio-file-sha1-checked,radio/models,0a5c3b2cef8f0c09b09f80363601f16491824a89)
-$(call add-radio-file-sha1-checked,radio/sboot.bin_SM-P610,da1c2208979489e3541332d9962f613e7470fc6b)
-$(call add-radio-file-sha1-checked,radio/uh.bin_SM-P610,821875f1f76c1d236689cf679b693d3f3c87fd80)
-$(call add-radio-file-sha1-checked,radio/up_param.bin_SM-P610,42bd804b453e7309312575f3b035f5b9d6f50f5a)
-$(call add-radio-file-sha1-checked,radio/version_SM-P610,b8adee974f3573d4112566860ae2dc2e6aeb0b5f)
+$(call add-radio-file-sha1-checked,radio/sboot.bin_SM-P610,5e1a94c0bf3172e84ecbe2328afec48f37787cf6)
+$(call add-radio-file-sha1-checked,radio/uh.bin_SM-P610,7cb5f2a8142421bd596b2da734c0ba54d2294ceb)
+$(call add-radio-file-sha1-checked,radio/up_param.bin_SM-P610,48eb7cdb957cff1536db0046a16f10c185df55b0)
+$(call add-radio-file-sha1-checked,radio/version_SM-P610,6f9ab9aa5b0a3e9ab460eb063c16bfc7d4dbc6c2)
 
 endif
